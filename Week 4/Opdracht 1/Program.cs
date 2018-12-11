79a1dfe0981329cea30915e3d9f1cfe23c182af5
Tomas van der Rijst <tomasvanderrijst@gmail.com>
2018-12-11 22:10:20 +0100

    week 4

---
+++ Week 4/Opdracht 1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+
+namespace Opdracht_1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+
+        void Start()
+        {
+            string fileName = "E:\\Users\\tomas\\Documents\\GitHub\\Programming-2\\Week 4\\Opdracht 1\\names.txt";
+            Person person = ReadPerson(fileName);
+            if(person.name == null)
+            {
+                Console.WriteLine("Welcome new guy");
+                person = ReadPerson();
+                WritePerson(person, fileName);
+            }
+            else
+            {
+                Console.WriteLine("Welcome back " + person.name);
+                Console.WriteLine("We have the next data stored");
+                ShowPerson(person);
+
+            }
+                                 
+            Console.ReadKey();
+        }
+
+        Person ReadPerson()
+        {
+            Person person = new Person();
+            Console.Write("What's your name?");
+            string name = Console.ReadLine();
+            Console.Write("Where do you live?");
+            string adress = Console.ReadLine();
+            Console.Write("How old are you?");
+            int age = int.Parse(Console.ReadLine());
+
+            person.age = age;
+            person.adress = adress;
+            person.name = name;
+
+            return person;
+        }
+
+        void WritePerson(Person p, string fileName)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName, true))
+            {
+
+                writer.WriteLine(p.name);
+                writer.WriteLine(p.adress);
+                writer.WriteLine(p.age);
+                Console.WriteLine("stored in " + fileName);
+            }
+        }
+
+        void ShowPerson(Person p)
+        {
+            Console.WriteLine("{0, -10} :{1, 10} ", "Name ",  p.name);
+            Console.WriteLine("{0, -10} :{1, 10} ", "Age ", p.age);
+            Console.WriteLine("{0, -10} :{1, 10} ", "Adress ", p.adress);
+
+        }
+
+        Person ReadPerson(string fileName)
+        {
+            Person person = new Person();
+
+            Console.WriteLine("Whats your name?");
+            string name = Console.ReadLine();
+            int counter = 0;
+            string line;
+
+            System.IO.StreamReader file =
+            new System.IO.StreamReader(fileName);
+            while ((line = file.ReadLine()) != null)
+            {
+                if(line == name)
+                {
+                    person.name = line;
+                    counter++;
+                    line = file.ReadLine();
+                    person.adress = line;
+                    counter++;
+                    line = file.ReadLine();
+                    person.age = int.Parse(line);
+                    break;
+                }
+                counter++;
+            }
+            
+
+            return person;
+        }
+
+
+    }
+
+    class Person
+    {
+        public string name;
+        public string adress;
+        public int age;
+    }
+}
---
+++ Week 4/Opdracht 2/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Opdracht_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+
+        void Start()
+        {
+            string fileName = "E:\\Users\\tomas\\Documents\\GitHub\\Programming-2\\Week 4\\Opdracht 2\\words.txt";
+            HangMan hangMan = new HangMan();
+            List<String> wordsList = hangMan.WordsList();
+            string word = SelectWord(wordsList);
+            hangMan.Init(word);
+            PlayHangMan(hangMan);
+
+            Console.WriteLine("Het geheime woord is: " + hangMan.secretWord);
+            Console.WriteLine("Het geraden woord is: " + hangMan.quessedWord);
+            Console.ReadKey();
+
+            
+        }
+
+        string SelectWord(List<string> words)
+        {
+            Random rnd = new Random();
+
+            int r = rnd.Next(words.Count);
+            string word = words[r];
+
+            return word;
+        }
+
+        bool PlayHangMan(HangMan hangMan)
+        {
+            List<char> inputChars;
+
+
+            if(hangMan.secretWord == hangMan.quessedWord)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
+
+    }
+
+    class HangMan
+    {
+        public string secretWord;
+        public string quessedWord;
+
+        
+
+        public void Init(string getSecretWord)
+        {
+            secretWord = getSecretWord;
+            int length = secretWord.Length;
+            int i = 0;
+            while (i < length)
+            {
+                quessedWord += ". ";
+                i++;
+            }
+
+                
+
+        }
+
+        public List<string> WordsList()
+        {
+            List<string> wordsList = new List<string>();
+            String filename = "E:\\Users\\tomas\\Documents\\GitHub\\Programming-2\\Week 4\\Opdracht 2\\words.txt";
+            StreamReader reader = File.OpenText(filename);
+            var lines = File.ReadAllLines(filename);
+            var r = new Random();
+
+            int i = 0;
+            string line;
+
+            while (i < 20)
+            {
+                var randomLineNumber = r.Next(0, lines.Length - 1);
+                line = lines[randomLineNumber];
+                if (line.Length >= 3)
+                {
+                    wordsList.Add(line);
+                    i++;
+                }
+            }
+
+            return wordsList;
+
+        }
+
+       
+
+    }
+}
